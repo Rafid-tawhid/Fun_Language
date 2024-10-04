aa5064fdcdb5dfbcd3e25db44492384a96f90c9f
rafid <rafidtawhid@gmail.com>
2024-10-05 01:20:58 +0600

    balsal korsi ami

--- lib/providers/post_provider.dart
+++ lib/providers/post_provider.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:my_messenger/models/user_model.dart';
 
 class PostProvider extends ChangeNotifier{
   Future<void> saveLikeInfo({
@@ -15,11 +16,13 @@ class PostProvider extends ChangeNotifier{
 
     try {
 
-      var data= await _firestore.collection('posts').doc(id).get();
-      await _firestore.collection('posts').doc(id).update({
-        'newKey': 'newValue':{"a":"dd"},  // This will add 'newField': 'newValue' to the document
+      // var data= await _firestore.collection('posts').doc(id).get();
+      DocumentReference documentReference=_firestore.collection('posts').doc(id).collection('likes').doc();
+      documentReference.set({
+        "docRef":documentReference.id,
+        "userId":userId,
+        "like":like
       });
-
 
     } catch (e) {
       // Handle errors
@@ -27,34 +30,39 @@ class PostProvider extends ChangeNotifier{
     }
   }
 
-  Future<int> getReactions(String lable, String postId) async {
+  // Function to get a like by document ID
+  Future<String> getLike(String postId,String userId) async {
+
+    String text='like';
+    List<Map<String, dynamic>> likesList = [];
+
     final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-    int totalLikes = 0;
-
     try {
-     if(lable=='Like'){
-       // Query the 'reactions' collection for the specific postId where 'likes' is true
-       QuerySnapshot snapshot = await _firestore
-           .collection('reactions')
-           .where('postId', isEqualTo: postId)
-           .where('likes', isEqualTo: true)
-           .get();
 
-       // The total number of likes is the number of documents that match the query
-       totalLikes = snapshot.size;
+      QuerySnapshot snapshot = await _firestore.collection('posts').doc(postId).collection('likes').get();
 
-       print('Total likes for postId $postId: $totalLikes');
-     }
+      // Iterate through each document in the snapshot
+      for (var doc in snapshot.docs) {
+        // Add each document's data to the likesList
+        likesList.add(doc.data() as Map<String, dynamic>);
+      }
+      for (var data in likesList){
+        if(data['userId']==userId){
+          debugPrint('found ${data['like']}');
+          text='liked';
+        }
+        else {
+          text='like';
+        }
+      }
+        print('Retrieved ${likesList.length} likes for postId: $postId');
     } catch (e) {
-      // Handle errors
-      print('Error retrieving likes: $e');
+      print('Error getting likes: $e');
+      text='like';
     }
 
-    return totalLikes;
+    return text;
   }
-
-
-
 
 
 }
